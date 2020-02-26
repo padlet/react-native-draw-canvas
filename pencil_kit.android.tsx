e8f0f4f23fc75c9f28f2c77627f4a555696d18ff
asleepace <colin_teahan@yahoo.com>
2020-02-25 17:33:39 -0800

    updating the android pencil kit

--- pencil_kit.android.tsx
+++ pencil_kit.android.tsx
@@ -1,14 +1,10 @@
 import React, { ReactElement } from 'react'
 import ReactNative, {
-  Button,
   processColor,
   requireNativeComponent,
-  ScrollView,
   StyleSheet,
   SyntheticEvent,
-  TouchableOpacity,
   UIManager,
-  View,
 } from 'react-native'
 
 // import the native android pencil kit class
@@ -16,53 +12,21 @@ const RCTPencilKitView = requireNativeComponent('RCTPencilKitView')
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-  frame: {
     justifyContent: 'center',
     backgroundColor: 'white',
     alignItems: 'baseline',
     flex: 1,
   },
-  buttons: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-  },
-  scrollView: {
-    backgroundColor: 'white',
-    flexShrink: 1,
-    flexGrow: 0,
-    padding: 4,
-  },
-  buttonFrame: {
-    flexDirection: 'row',
-  },
-  button: {
-    padding: 4,
-  },
-  color: {
-    height: 40,
-    width: 40,
-  },
-  border: {
-    borderWidth: 2,
-    borderRadius: 2,
-    borderColor: '#FDD230',
-    padding: 2,
-  },
-  row: {
-    flexDirection: 'row',
-    backgroundColor: 'white',
-  },
 })
 
 interface Props {
   style?: 'light' | 'dark'
+  color?: 'string'
 }
 interface State {
   color: string
-  tool: 'penil' | 'eraser'
 }
+
 type SaveCallback = ({ uri: string }) => void
 
 const COLORS = [
@@ -79,8 +43,8 @@ const COLORS = [
 ]
 
 export class PencilKit extends React.Component<Props, State> {
-  private pencilKit: React.RefObject<PencilKit>
 
+  private pencilKit: React.RefObject<PencilKit>
   private onSaveCallback: SaveCallback = null
 
   // Always true on android
@@ -92,9 +56,13 @@ export class PencilKit extends React.Component<Props, State> {
     super(props)
     this.pencilKit = React.createRef()
     this.state = {
-      color: 'black',
-      tool: 'penil',
+      color: props.color || 'black',
     }
+  }
+
+  static getDerivedStateFromProps(props:Props, state:State): State {
+    const color = props.color || state.color
+    return { ...state, color }
   }
 
   public setColor = (color: string): void => {
@@ -134,45 +102,13 @@ export class PencilKit extends React.Component<Props, State> {
   }
 
   render(): ReactElement {
-    const color = processColor(this.state.color)
-    console.log('[APencilKit] android color:', color)
     return (
       <RCTPencilKitView
-        style={styles.frame}
-        color={color}
-        ref={this.pencilKit}
+        color={processColor(this.state.color)}
         onSaveEvent={this.onSaveEvent}
+        style={styles.container}
+        ref={this.pencilKit}
       />
     );
   }
-}
-
-interface PickerProps {
-  action: Function
-  current: string
-}
-function ColorPicker(props: PickerProps): ReactElement {
-  const { current, action } = props
-  return (
-    <ScrollView horizontal={true} style={styles.scrollView} contentContainerStyle={styles.buttonFrame}>
-      {COLORS.map((color, i) => (
-        <ColorButton color={color} key={i} current={current} action={action} />
-      ))}
-    </ScrollView>
-  )
-}
-
-interface ButtonProps {
-  action: Function
-  current: string
-  color: string
-}
-function ColorButton(props: ButtonProps): ReactElement {
-  const onPress = (): void => props.action(props.color)
-  const borders = props.color === props.current ? styles.border : null
-  return (
-    <TouchableOpacity style={[styles.button, borders]} onPress={onPress}>
-      <View style={[styles.color, { backgroundColor: props.color }]} />
-    </TouchableOpacity>
-  )
 }
